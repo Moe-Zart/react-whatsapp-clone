73557a8710273cf235a4dedbd4080d682900a975
Moe-Zart <mohamedzreika03@outlook.com>
2025-03-05 17:27:59 +1100

    startercode more updated

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import "./sidebar.css"
+import PersonalInfo from './personalInfo/PersonalInfo';
+import ChatList from './chatList/ChatList';
 
 const Sidebar = () => {
     return (
         <div className='sidebar'>
             Sidebar
+            <PersonalInfo/>
+            <ChatList/>
         </div>
     );
 }
---
+++ src/components/sidebar/personalInfo/PersonalInfo.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./personalInfo.css"
+
+const PersonalInfo = () => {
+    return (
+        <div>
+            - PersonalInfo
+        </div>
+    );
+}
+
+export default PersonalInfo;
